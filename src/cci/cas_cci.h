b8010cbb57a9875b3bcf78f6fb9251b2ce48e437
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2026-04-17 10:17:29 +0900

    support copy protocol in cci

--- src/cci/cas_cci.h
+++ src/cci/cas_cci.h
@@ -980,6 +980,9 @@ extern "C"
   extern int cci_is_shard (int con_h_id, T_CCI_ERROR * err_buf);
   extern int cci_get_cas_info (int mapped_conn_id, char *info_buf, int buf_length, T_CCI_ERROR * err_buf);
 
+  extern int cci_copy_send_data (int mapped_conn_id, const char *data, int data_len, T_CCI_ERROR * err_buf);
+  extern int cci_copy_end (int mapped_conn_id, T_CCI_ERROR * err_buf);
+
 #ifdef __cplusplus
 }
 #endif
